4e7ac7efa083cae98282b74ce5dea8fd4309a1ca
Nawfal Mahdi <nawfal.mahdi@hotmail.com>
2015-05-28 02:04:22 +1000

    create HTML form

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
     /*
-                    Based - in part - on the HelloWorld sample extension on the Brackets wiki:
-                    https://github.com/adobe/brackets/wiki/Simple-%22Hello-World%22-extension
-                    */
+                                                                                                                                                                        Based - in part - on the HelloWorld sample extension on the Brackets wiki:
+                                                                                                                                                                        https://github.com/adobe/brackets/wiki/Simple-%22Hello-World%22-extension
+                                                                                                                                                                        */
     define(function (require, exports, module) {
 
         var CommandManager = brackets.getModule("command/CommandManager"),
@@ -16,21 +16,27 @@
 
         function handleHelloWorld() {
             var str = "";
-            str += '<form action="demo_form.asp">  First name: <input type="text" name="fname"><br>  Last name: <input type="text" name="lname"><br>  <input type="submit" value="Submit"> < /form>';
-            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, "Hello World", str);
+            str += '<div class="inline-editor-header">\
+  <button class="btn btn-mini no-focus btn-shorthand-done" title="Copy changes back to code">Done</button>\
+  <button class="btn btn-mini no-focus btn-shorthand-cancel" title="Cancel changes">Cancel</button>\
+</div>\
+<div class="inline-editor-holder"></div>';
+            console.log('we are going to show the dialog');
+            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, "ENG1003 Assignment Uploader-V0.1", str);
+            window.alert($("#fname").val());
+
+            function test() {
+                console.log('we got the message');
+                window.alert("this is test");
+            }
         }
 
+
         AppInit.appReady(function () {
-
-            log("Hello from HelloWorld3.");
-
-            var HELLOWORLD_EXECUTE = "helloworld.execute";
-
-            CommandManager.register("Run HelloWorld", HELLOWORLD_EXECUTE, handleHelloWorld);
-
+            log("Upload Assignement Extenstion");
+            var HELLOWORLD_EXECUTE = "ENG1003Uploader.monash";
+            CommandManager.register("ENG1003 Uploader", HELLOWORLD_EXECUTE, handleHelloWorld);
             var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
             menu.addMenuItem(HELLOWORLD_EXECUTE);
-
         });
-
     });
