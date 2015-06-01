33dc0a180037d89a4ba0efb51ea70e5a5aeedb85
Nawfal Mahdi <nawfal.mahdi@hotmail.com>
2015-06-01 11:40:51 +1000

    Update Todo file

--- todo.js
+++ todo.js
@@ -1,12 +1,14 @@
-
-Step#3: Call the server...
-Step#4: Testing
-Step#5: Deploy
-Step#6: Thats it... Go home...
+Step #3.1: Pushing Code to the server
+Step# 3.2: Error Handling
+Step #3.3: Log file
+Step# 4: Testing
+Step #5: Deploy
+Step# 6: Thats it...Go home...
 
 
 Done:
-Step#1: Create UI
-Step#2: Assign functions
-Step#2.1: Descriptive Variables  Both Dialog and main.js
-Step#2.2: Get the current document
+    Step #1: Create UI
+Step# 2: Assign functions
+Step #2.1: Descriptive Variables  Both Dialog and main.js
+Step# 2.2: Get the current document
+Step #3: Call the server...
